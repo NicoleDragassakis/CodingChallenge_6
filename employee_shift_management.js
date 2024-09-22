0a4fd57d18f8060f5072dc18b9a386a6decc630d
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-22 14:49:45 -0400

    Initialize employees with shift schedules - Task 1

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,15 +1,35 @@
 //Task Zero
 
-//initialization of Employees
+//initialization of Employees this was given on the assignment page
+
+//const employees = [
+
+//{ name: 'John', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
+    
+//{ name: 'Sara', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
+    
+//{ name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
+    
+//{ name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
+    
+//];
+
+//Task one
+
+console.log("Task one"); //add for better console readability
 
 const employees = [
 
-{ name: 'John', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
-    
-{ name: 'Sara', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
-    
-{ name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
-    
-{ name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
-    
-];
+    { name: 'John', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
+        
+    { name: 'Sara', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
+        
+    { name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
+        
+    { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] },
+
+    { name: 'Jessie', shifts:[{ day:"Saturday", hours: 10}, {day: "Sunday", hours: 8 }] }
+        
+    ];
+
+    console.log(employees);
