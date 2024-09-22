0eeb3cbdb03b87372e02936e28610b2fd64bafbc
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-22 15:18:19 -0400

    Create displayEmployeeShifts function - Task two

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -32,4 +32,26 @@ const employees = [
         
     ];
 
-    console.log(employees);
+    console.log("Employee Data:",employees);
+
+    //Task Two
+
+    console.log("Task Two"); //add for better console readability
+
+    function displayEmployeeShifts(employees, name) {
+        employees.forEach(employee => {
+            if (employee.name === name) {
+                console.log('${employee.name}'); //the '' makes it work?
+                employee.shifts.forEach(shift => {
+                    console.log(`Assigned Shifts: ${shift.day}, Hours Worked: ${shift.hours}`);
+                });
+            }
+        });
+    }
+
+//displaying employee shift data
+displayEmployeeShifts(employees, "John");
+displayEmployeeShifts(employees, "Sara");
+displayEmployeeShifts(employees, "David");
+displayEmployeeShifts(employees, "Emily");
+displayEmployeeShifts(employees, "Jessie");
